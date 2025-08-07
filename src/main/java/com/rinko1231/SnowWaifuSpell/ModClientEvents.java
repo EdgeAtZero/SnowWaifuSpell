51d9ce4f2260fdf6bdbbdba7a05b3efe151f5dcf
Rinko1231 <89371859+Rinko1231@users.noreply.github.com>
2025-08-07 11:52:25 +0800

    1.0.1

--- src/main/java/com/rinko1231/SnowWaifuSpell/ModClientEvents.java
+++ src/main/java/com/rinko1231/SnowWaifuSpell/ModClientEvents.java
@@ -17,30 +17,26 @@ import static com.rinko1231.SnowWaifuSpell.SnowWaifuSpell.MOD_ID;
 
 @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModClientEvents {
-    public static final ModelLayerLocation NEW_SNOW_WAIFU_LAYER = new ModelLayerLocation(
-            ResourceLocation.fromNamespaceAndPath(MOD_ID, "snow_waifu"),
-            "main"
-    );
-
     public static final ModelLayerLocation SNOW_WAIFU_LAYER = new ModelLayerLocation(
-            ResourceLocation.fromNamespaceAndPath(MOD_ID, "new_snow_waifu"),
+            ResourceLocation.fromNamespaceAndPath(MOD_ID, "snow_waifu"),
             "main"
     );
 
     @SubscribeEvent
     public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        final boolean jappa = JappaPackReloadListener.INSTANCE.isJappaPackLoaded();
         event.registerEntityRenderer(
                 ModEntityRegistry.SUMMONED_SNOW_QUEEN.get(),
-                !jappa ?
-                        c -> new SummonedSnowQueenRenderer(c, new SummonedSnowQueenModel(c.bakeLayer(SNOW_WAIFU_LAYER))) :
-                        c -> new NewSummonedSnowQueenRenderer(c, new NewSummonedSnowQueenModel(c.bakeLayer(NEW_SNOW_WAIFU_LAYER)))
+                c -> new SummonedSnowQueenRenderer(c, new SummonedSnowQueenModel(c.bakeLayer(SNOW_WAIFU_LAYER)))
         );
     }
 
     @SubscribeEvent
     public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
-        event.registerLayerDefinition(SNOW_WAIFU_LAYER, SummonedSnowQueenModel::create);
-        event.registerLayerDefinition(NEW_SNOW_WAIFU_LAYER, NewSummonedSnowQueenModel::create);
+        event.registerLayerDefinition(SNOW_WAIFU_LAYER, () -> {
+            // 动态判断 Jappa 资源包状态
+            return JappaPackReloadListener.INSTANCE.isJappaPackLoaded()
+                    ? NewSummonedSnowQueenModel.create()   // Jappa 模型
+                    : SummonedSnowQueenModel.create();     // 默认模型
+        });
     }
 }
